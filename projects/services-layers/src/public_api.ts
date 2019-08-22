1bf62ea75f9fb31f4b434a30b8fa5afe465076eb
boec_mt <Mathias.Boeck@dlr.de>
2019-08-22 13:15:47 +0200

    remove not used and update dependencies

--- projects/services-layers/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of layers
- */
-export * from './lib/types/Layers';
-export * from './lib/types/LayerGroup';
-
-export * from './lib/layers.service';
-export * from './lib/layers.module';

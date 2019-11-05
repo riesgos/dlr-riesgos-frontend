b41f304d03370ebbe21dad113f70ebf6288b7308
Langbein <Michael.Langbein@dlr.de>
2019-11-05 10:03:41 +0100

    some formatting

--- projects/services-wps/src/lib/utils/cache.ts
+++ projects/services-wps/src/lib/utils/cache.ts
@@ -2,23 +2,23 @@ import { UtilStoreService } from '@ukis/services-util-store';
 import md5 from 'md5';
 
 export class Cache {
-    
-    private store: UtilStoreService
-    
+
+    private store: UtilStoreService;
+
     constructor() {
         this.store = new UtilStoreService();
     }
 
     get(key: string): any {
         const response = this.store.local(key);
-        //if(response) console.log("found this in cache for key "+key+": ", response);
-        //else console.log("No cache hit for key "+key);
+        // if(response) console.log("found this in cache for key "+key+": ", response);
+        // else console.log("No cache hit for key "+key);
         return response;
     }
 
     set(key: string, response: any): void {
         this.store.local(key, response);
-        //console.log("stored in cache for key " + key + ": ", response);
+        // console.log("stored in cache for key " + key + ": ", response);
     }
 
     public makeKey(object: any): string {

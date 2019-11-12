546e470844dc47c2765090aa5143275dbb675bce
Langbein <Michael.Langbein@dlr.de>
2019-11-12 15:55:39 +0100

    something about 'import * as d3base from d3; const d3 = Object.assign(d3base, d3g);' causes production build to break

--- src/app/components/graphvizcomp/graphvizcomp.component.ts
+++ src/app/components/graphvizcomp/graphvizcomp.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, SimpleChanges, Self } from '@angular/core';
-import * as d3base from 'd3';
+import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, SimpleChanges } from '@angular/core';
 import * as d3g from 'd3-graphviz';
-const d3 = Object.assign(d3base, d3g);
 
 @Component({
   selector: 'app-graphvizcomp',
@@ -14,9 +12,7 @@ export class GraphvizcompComponent implements OnChanges {
 
   @Input() dotString: string;
 
-  constructor(
-    // @Self() private element: ElementRef
-    ) {
+  constructor() {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -29,14 +25,11 @@ export class GraphvizcompComponent implements OnChanges {
   private createGraph(): void {
     const element = this.container.nativeElement;
     const dotString = this.dotString;
-    const svg = d3.select(element)
-      // .style('width', `${this.element.nativeElement.offsetWidth}px`)
-      // .style('height', `${this.element.nativeElement.offsetHeight}px`)
-        .graphviz({
-          height: 600,
-          width: 800,
-          fit: true
-        }).renderDot(dotString);
+    const svg = d3g.graphviz(element)
+      .width(800)
+      .height(600)
+      .fit(true)
+      .renderDot(dotString);
   }
 
 

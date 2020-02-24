783a9ffbfb4857befd222396cb6d002db36ff856
Langbein <Michael.Langbein@dlr.de>
2020-02-24 10:11:55 +0100

    removed unneccessary imports

--- @ukis/lib/wmts/wmtsclient.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-export declare class WmtsClientService {
-    private http;
-    private xmlmarshaller;
-    private xmlunmarshaller;
-    constructor(http: HttpClient);
-    getCapabilities(url: string, version?: string): Observable<object>;
-}
--- @ukis/lib/wps/utils/polling.d.ts
+++
@@ -1,3 +0,0 @@
-import { Observable } from 'rxjs';
-export declare function pollUntil<T>(task$: Observable<T>, predicate: (results: any) => boolean, doWhile?: (t: T | null) => any, minWaitTime?: number): Observable<T>;
-export declare function delayedRetry(delayMs: number, maxRetries?: number): (src$: Observable<any>) => Observable<any>;

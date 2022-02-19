1fd779df42d342ad6b2cdfefd3f795e3afecfd1e
Langbein, Michael <michael.langbein@dlr.de>
2022-02-19 18:04:45 +0100

    updated to angular v12. still need to update clr12 and ukis8

--- src/app/riesgos/riesgos.integration.spec.ts
+++ src/app/riesgos/riesgos.integration.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { XhrFactory, HttpClient, HttpXhrBackend } from '@angular/common/http';
+import { XhrFactory } from '@angular/common';
+import { HttpClient, HttpXhrBackend } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { effects, reducers } from '../ngrx_register';

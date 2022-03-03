00acc50ea5de8003e404a1d3d56f51fae6380194
boec_mt <Mathias.Boeck@dlr.de>
2022-03-03 18:17:53 +0100

    change disclaimer altert to header

--- src/app/components/disclaimer/disclaimer.component.ts
+++ src/app/components/disclaimer/disclaimer.component.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, UrlSegment } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { AlertService } from '../global-alert/alert.service';
 
 @Component({
   selector: 'ukis-disclaimer',
   templateUrl: './disclaimer.component.html',
   styleUrls: ['./disclaimer.component.scss']
 })
-export class DisclaimerComponent implements OnInit {
-
-
+export class DisclaimerComponent implements OnInit, OnDestroy {
   public isExpanded: boolean;
+  onLangChange: Subscription;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private translate: TranslateService, private alertService: AlertService) {
     this.isExpanded = environment.production ? true : false;
   }
 
   ngOnInit() {
-    // this.route.queryParams.subscribe((data) => {
-    //   if (data['id']) {
-    //     this.isExpanded = true;
-    //   } else {
-    //     this.isExpanded = false;
-    //   }
-    // });
+    if (this.isExpanded) {
+      this.translate.get('This is a demonstrator!').pipe(first()).subscribe(() => {
+        this.setAlert();
+      })
+    }
+  }
+  ngOnDestroy(): void {
+    if(this.onLangChange){
+      this.onLangChange.unsubscribe();
+    }
+  }
+
+  setAlert() {
+    this.alertService.alert({
+      type: 'info',
+      text: this.translate.instant('This is a demonstrator!'),
+      closeable: true,
+      closeAction: () => { this.isExpanded = false; }
+    });
+    this.isExpanded = true;
+  }
+
+  toggleAlert() {
+    if (!this.isExpanded) {
+      this.setAlert();
+
+      this.onLangChange = this.translate.onLangChange.subscribe(() => {
+        console.log('change')
+        this.setAlert();
+      });
+    } else {
+      if(this.onLangChange){
+        this.onLangChange.unsubscribe();
+      }
+      this.alertService.alert(null);
+      this.isExpanded = false;
+    }
   }
 }

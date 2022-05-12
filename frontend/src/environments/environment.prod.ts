1b82490f6aef99b6f9a9ecfa001bf70dec2ae4fb
Langbein, Michael <michael.langbein@dlr.de>
2022-05-12 15:53:44 +0200

    fallback-proxy no longer needed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   middlewareUrl: 'https://riesgos.dlr.de/middleware',
   useProxy: true,
   proxyUrl: 'https://riesgos.dlr.de/proxy',
+  useFallbackProxy: false,
   fallbackProxyUrl: 'https://hexaph.one/proxy',
   gfzUseStaging: false,
 };
--- frontend/src/environments/environment.staging.ts
+++ frontend/src/environments/environment.staging.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   middlewareUrl: 'https://riesgos.dlr.de/middleware',
   proxyUrl: 'https://riesgos.dlr.de/proxy',
-  fallbackProxyUrl: 'https://hexaph.one/proxy',
   useProxy: true,
+  useFallbackProxy: false,
+  fallbackProxyUrl: 'https://hexaph.one/proxy',
   gfzUseStaging: false, // this would cause requests to 8443, which DLR won't allow.
 };

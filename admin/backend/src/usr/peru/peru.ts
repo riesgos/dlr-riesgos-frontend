df008b7bd8f6608ca0bcc2083b9bfa129807abea
Michael Langbein <michael_langbein@hotmail.de>
2022-10-20 15:06:24 +0200

    added eq-simulation service

--- admin/backend/src/usr/peru/peru.ts
+++ admin/backend/src/usr/peru/peru.ts
@@ -1,6 +1,7 @@
 import { ScenarioFactory } from "../../scenarios/scenarios";
 import { step as eqs } from "./1_eqs/eqs";
 import { step as select } from "./2_eqselection/eqselection";
+import { step as eqSim } from './3_eqsim/eqsim';
 
 export const peruFactory = new ScenarioFactory(
     'Peru',
@@ -9,3 +10,4 @@ export const peruFactory = new ScenarioFactory(
 
 peruFactory.registerStep(eqs);
 peruFactory.registerStep(select);
+peruFactory.registerStep(eqSim);

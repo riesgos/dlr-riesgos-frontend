a759371c7aa66a067e54a3fb50975e1520145897
Michael Langbein <michael_langbein@hotmail.de>
2022-11-14 17:06:03 +0100

    catching and reporting errors

--- admin/backend/src/scenarios/pool.ts
+++ admin/backend/src/scenarios/pool.ts
@@ -13,6 +13,7 @@ export class ProcessPool {
 
     private ongoing: string[] = [];
     private completed: {[hash: string]: any} = {};
+    private failed: {[hash: string]: any} = {};
 
     public scheduleTask(key: string, task: Task): { ticket: string } {
         if (this.ongoing.includes(key)) return { ticket: key };
@@ -20,11 +21,23 @@ export class ProcessPool {
         task().then(results => {
             this.ongoing = this.ongoing.filter(h => h !== key);
             this.completed[key] = results;
+        }).catch(error => {
+            this.ongoing = this.ongoing.filter(h => h !== key);
+            this.failed[key] = error;
         });
         return { ticket: key };
     }
 
     poll(ticket: string): { error: string } | { ticket: string } | { results: any } {
+        const error = this.failed[ticket];
+        if (error) {
+            delete this.failed[ticket];
+            return {
+                // Error objects are not properly stringified by default.
+                error: JSON.stringify(error, Object.getOwnPropertyNames(error))
+            };
+        }
+
         const data = this.completed[ticket];
         if (data) {
             delete this.completed[ticket];

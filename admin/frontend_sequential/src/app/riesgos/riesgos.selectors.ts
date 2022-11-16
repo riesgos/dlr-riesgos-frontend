aff935100b952361323aea0d30f3dc5f6ae8680b
Michael Langbein <michael_langbein@hotmail.de>
2022-11-16 17:34:31 +0100

    removed frontend-seq and frontend-cat

--- admin/frontend_sequential/src/app/riesgos/riesgos.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as fromRiesgos from './riesgos.reducer';
-
-export const selectRiesgosState = createFeatureSelector<fromRiesgos.RiesgosState>(
-  fromRiesgos.riesgosFeatureKey
-);

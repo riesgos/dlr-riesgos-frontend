90860da8e9dd407d116e5e66b8bbace11799a741
Michael Langbein <michael_langbein@hotmail.de>
2022-11-17 15:41:50 +0100

    forgot to update interactions to new api

--- frontend/src/app/interactions/interactions.effects.ts
+++ frontend/src/app/interactions/interactions.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
-import { InteractionAction, InteractionActionTypes, InteractionCompleted } from './interactions.actions';
+import * as InteractionActions from './interactions.actions';
 import * as RiesgosActions from '../riesgos/riesgos.actions';
 import { map } from 'rxjs/operators';
 
@@ -11,9 +11,9 @@ export class InteractionEffects {
 
     $interactionCompleted = createEffect(() => {
         return this.actions$.pipe(
-            ofType<InteractionAction>(InteractionActionTypes.completed),
-            map((action: InteractionCompleted) => {
-                return RiesgosActions.productsProvided({products: [action.payload.product]});
+            ofType(InteractionActions.interactionCompleted),
+            map(action => {
+                return RiesgosActions.productsProvided({products: [action.product]});
             })
         );
     });

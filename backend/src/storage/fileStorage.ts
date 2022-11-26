0a67e692ef8cb4029827dd648b7e159a0cbaa8dc
Michael Langbein <michael_langbein@hotmail.de>
2022-11-26 16:54:32 +0100

    backend: maximum cache age

--- backend/src/storage/fileStorage.ts
+++ backend/src/storage/fileStorage.ts
@@ -1,11 +1,14 @@
 import objectHash from 'object-hash';
-import { pathJoin, readJsonFile, writeJsonFile } from '../utils/files';
+import { deleteFile, getFileAge, pathJoin, readJsonFile, writeJsonFile } from '../utils/files';
 
 
 
 export class FileStorage<Properties extends {}> {
 
-    constructor(private filePath: string) {
+    constructor(
+        private filePath: string,
+        private maxCacheLifetime = 3 * 24 * 60 * 60
+    ) {
         // @TODO: also store props in a database.
         // This way users can query files by only parts of the props-object,
         // and they can restore properties from a hash.
@@ -21,6 +24,12 @@ export class FileStorage<Properties extends {}> {
     public async getDataByKey<T>(key: string): Promise<T | undefined> {
         const fullFilePath = pathJoin([this.filePath, key]);
         try {
+            const age = await getFileAge(fullFilePath);
+            if (age > this.maxCacheLifetime) {
+                await deleteFile(fullFilePath);
+                return undefined;
+            }
+
             const contents = await readJsonFile(fullFilePath);
             console.log(`Store: fetched data from cache: ${key}`);
             return contents;

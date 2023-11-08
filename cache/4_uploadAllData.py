412f83578b02599a99679ef41af23941285ba424
michaellangbein <michael_langbein@hotmail.de>
2023-11-08 12:33:46 +0100

    reverse proxy

--- cache/4_uploadAllData.py
+++ cache/4_uploadAllData.py
@@ -277,7 +277,7 @@ def uploadAll():
 
 
 #%%
-geoserverUrl= "http://localhost:8003/geoserver"
+geoserverUrl= "http://10.17.192.96:8080/geoserver"
 geo = MyGeoserver(geoserverUrl, username="admin", password="geoserver")
 uploadAll()
 
